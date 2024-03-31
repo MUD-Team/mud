60bf578c8118f90a5e1d5e33762f8362ef4ecbdb
PaleBlueDot <125748236+pbdot@users.noreply.github.com>
2024-03-31 11:20:17 -0400

    Fix clang format issue with win32 crash handler

--- source/engine/common/i_crash_win32.cpp
+++ source/engine/common/i_crash_win32.cpp
@@ -30,7 +30,7 @@
 #if defined _WIN32 && defined _MSC_VER && !defined _DEBUG
 
 #define CRASH_DIR_LEN 1024
-
+#include "win32inc.h"
 #include <DbgHelp.h>
 #include <new.h>
 #include <signal.h>
@@ -41,7 +41,7 @@
 #include "i_system.h"
 #include "m_fileio.h"
 #include "odamex.h"
-#include "win32inc.h"
+
 
 /**
  * @brief An array containing the directory where crashes are written to.
