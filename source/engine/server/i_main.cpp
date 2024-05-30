e78b25f72381d25f7a2544718e1b8b244185bf48
PaleBlueDot <125748236+pbdot@users.noreply.github.com>
2024-05-30 16:13:50 -0400

    Fix linux server build

--- source/engine/server/i_main.cpp
+++ source/engine/server/i_main.cpp
@@ -327,10 +327,10 @@ int32_t main(int32_t argc, char **argv)
     {
         if (LOG.is_open())
         {
-            LOG << "=== ERROR: " << error.GetMsg() << " ===\n\n";
+            LOG << "=== ERROR: " << error.message() << " ===\n\n";
         }
 
-        fprintf(stderr, "=== ERROR: %s ===\n\n", error.GetMsg().c_str());
+        fprintf(stderr, "=== ERROR: %s ===\n\n", error.message().c_str());
 
         call_terms();
         PHYSFS_deinit();
